7a151989487b50eaa274a2fd977be9d0d740a619
John Meyer <j.meyer@live.com>
2021-06-09 22:20:14 -0400

    Worked as a snap in VM

--- TestOpenPA/Program.cs
+++ TestOpenPA/Program.cs
@@ -9,9 +9,15 @@ namespace TestOpenPA
 {
     class Program
     {
+#if DEBUG
+        static string addr = "tcp:10.1.10.102";
+#else
+        static string addr = "unix:/run/user/1000/pulse/native";
+#endif
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Connecting to {0}", addr);
 
             Task t = program();
             t.Wait();
@@ -24,7 +30,7 @@ namespace TestOpenPA
             PulseAudio.Init();            
 
             PAContext context = new(MainLoop.Instance, "TestApp");
-            var t = await context.ConnectAsync("tcp:10.1.10.102");
+            var t = await context.ConnectAsync(addr);
             
 
             Console.WriteLine("Connection succeeded: {0}", t);
