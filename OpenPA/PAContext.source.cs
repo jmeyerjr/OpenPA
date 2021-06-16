7ec81e6fa74cb90e85847a334e91450ec087df91
John Meyer <j.meyer@live.com>
2021-06-15 21:35:21 -0400

    Source introspection complete

--- OpenPA/PAContext.source.cs
+++ OpenPA/PAContext.source.cs
@@ -7,6 +7,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using static OpenPA.Native.pa_operation;
 
 namespace OpenPA
 {
@@ -15,15 +16,8 @@ namespace OpenPA
 
 
         #region Source
-        /// <summary>
-        /// State flag for the get_source_info callback
-        /// </summary>
-        static CallbackState gotSource;
-        /// <summary>
-        /// Structure to hold the source_info from the callback
-        /// </summary>
-        static pa_source_info* source_info;
 
+        #region Callback
         /// <summary>
         /// Callback for the get_source_info family of calls
         /// </summary>
@@ -34,99 +28,96 @@ namespace OpenPA
         [UnmanagedCallersOnly(CallConvs = new Type[] { typeof(CallConvCdecl) })]
         static unsafe void SourceCallback(pa_context* ctx, pa_source_info* info, int eol, void* userdata)
         {
-            pa_threaded_mainloop* m = (pa_threaded_mainloop*)userdata;
 
             // Test for the end of list
-            if (eol == 0 && info != null)
+            if (eol == 0)
             {
                 // Not the end of the list
 
-                // Copy the source_info address into the static pointer
-                source_info = info;
-
-                // Flag that there is valid data
-                gotSource = CallbackState.HasData;
+                // Copy the pa_source_info address
+                *((pa_source_info**)userdata) = info;
 
                 // Signal the mainloop to continue
-                pa_threaded_mainloop.pa_threaded_mainloop_signal(m, 1);
-            }
-            else if (eol < 0)
-            {
-                gotSource = CallbackState.Failed;
-                pa_threaded_mainloop.pa_threaded_mainloop_signal(m, 0);
+                MainLoop.Instance.Signal(1);
             }
             else
             {
-                gotSource = CallbackState.Success;
-                pa_threaded_mainloop.pa_threaded_mainloop_signal(m, 0);
+                // Return null
+                *((pa_source_info**)userdata) = null;
+
+                // Signal the mainloop
+                MainLoop.Instance.Signal(0);
             }
 
         }
+        #endregion
 
         /// <summary>
         /// Gets a SourceInfo object describing a source
         /// </summary>
         /// <param name="source">Name of the sink</param>
         /// <returns>SourceInfo object</returns>
-        public Task<SourceInfo?> GetSourceInfoAsync(string source)
+        public Task<SourceInfo?> GetSourceInfoAsync(string source) => Task.Run(() => GetSourceInfo(source));
+
+        /// <summary>
+        /// Gets a SourceInfo object describing a source
+        /// </summary>
+        /// <param name="source">Name of the sink</param>
+        /// <returns>SourceInfo object</returns>
+        public SourceInfo? GetSourceInfo(string source)
         {
-            if (_mainLoop == null)
-                throw new InvalidOperationException("MainLoop is null");
+            // Returned object
+            SourceInfo? info = default;
 
-            return Task.Run(() =>
+            // Lock the context so that we can remain thread-safe
+            Monitor.Enter(this);
+
+            // Copy the name of the sink to an unmanaged buffer
+            IntPtr name = Marshal.StringToHGlobalAnsi(source);
+
+            // Lock the mainloop
+            MainLoop.Instance.Lock();
+
+            // Pointer to returned structure
+            pa_source_info* source_info = null;
+
+            // Call the native get_source_info native function passing in the name and callback
+            pa_operation* op = pa_context.pa_context_get_source_info_by_name(pa_Context, name, &SourceCallback, &source_info);
+
+            // Wait for the callback to signal
+            while (pa_operation_get_state(op) == Enums.OperationState.RUNNING && source_info == null)
+                MainLoop.Instance.Wait();
+
+
+            // If the callback returned data
+            if (source_info != null)
             {
-                SourceInfo? info = default;
+                // Copy the unmanaged source_info structure into a SourceInfo object
+                info = SourceInfo.Convert(*source_info);
 
-                // Lock the context so that we can remain thread-safe
-                Monitor.Enter(this);
+                // Allow PulseAudio to free the pa_source_info structure
+                MainLoop.Instance.Accept();
 
-                // Copy the name of the sink to an unmanaged buffer
-                IntPtr name = Marshal.StringToHGlobalAnsi(source);
+                // Wait for the operation to complete
+                MainLoop.Instance.Wait();
 
-                // Set flag to 'pending'
-                gotSource = CallbackState.Pending;
+            }
 
-                // Lock the mainloop
-                _mainLoop.Lock();
+            // Dereference the pa_operation
+            pa_operation_unref(op);
 
+            // Unlock the mainloop
+            MainLoop.Instance.Unlock();
 
-                // Call the native get_source_info native function passing in the name and callback
-                pa_operation* op = pa_context.pa_context_get_source_info_by_name(pa_Context, name, &SourceCallback, _mainLoop.ptr);
+            // Free the unmanaged memory that holds the source name
+            Marshal.FreeHGlobal(name);
 
-                // Wait for the callback to signal
-                while (gotSource == CallbackState.Pending)
-                    _mainLoop.Wait();
+            // Unlock the context
+            Monitor.Exit(this);
 
+            // Return the SourceInfo object
+            return info;
 
-                // If the callback returned data
-                if (gotSource == CallbackState.HasData)
-                {
-                    // Copy the unmanaged source_info structure into a SourceInfo object
-                    info = SourceInfo.Convert(*source_info);
-
-                    // Allow PulseAudio to free the source_info
-                    _mainLoop.Accept();
-
-                    // Wait for the operation to complete
-                    _mainLoop.Wait();
-
-                }
-
-                // Dereference the pa_operation
-                pa_operation.pa_operation_unref(op);
-
-                // Unlock the mainloop
-                _mainLoop.Unlock();
-
-                // Free the unmanaged memory that holds the source name
-                Marshal.FreeHGlobal(name);
-
-                // Unlock the context
-                Monitor.Exit(this);
-
-                // Return the SourceInfo object
-                return info;
-            });
         }
 
         /// <summary>
@@ -134,127 +125,132 @@ namespace OpenPA
         /// </summary>
         /// <param name="source">Index of the source</param>
         /// <returns>SourceInfo object</returns>
-        public Task<SourceInfo?> GetSourceInfoAsync(uint index)
+        public Task<SourceInfo?> GetSourceInfoAsync(uint index) => Task.Run(() => GetSourceInfo(index));
+
+        /// <summary>
+        /// Gets a SourceInfo object describing a source
+        /// </summary>
+        /// <param name="source">Index of the source</param>
+        /// <returns>SourceInfo object</returns>
+        public SourceInfo? GetSourceInfo(uint index)
         {
-            if (_mainLoop == null)
-                throw new InvalidOperationException("MainLoop is null");
+            // Returned object
+            SourceInfo? info = default;
 
-            return Task.Run(() =>
+            // Lock the context so that we can remain thread-safe
+            Monitor.Enter(this);
+
+            // Lock the mainloop
+            MainLoop.Instance.Lock();
+
+            // Pointer to the returned structure
+            pa_source_info* source_info = null;
+
+            // Call the native get_source_info native function passing in the name and callback
+            pa_operation* op = pa_context.pa_context_get_source_info_by_index(pa_Context, index, &SourceCallback, &source_info);
+
+            // Wait for the callback to signal
+            while (pa_operation_get_state(op) == Enums.OperationState.RUNNING && source_info == null)
+                MainLoop.Instance.Wait();
+
+            // If the callback returned data
+            if (source_info != null)
             {
-                SourceInfo? info = default;
+                // Copy the unmanaged source_info structure into a SourceInfo object
+                info = SourceInfo.Convert(*source_info);
 
-                // Lock the context so that we can remain thread-safe
-                Monitor.Enter(this);
+                // Allow PulseAudio to free the source_info
+                MainLoop.Instance.Accept();
 
-                // Set flag to 'pending'
-                gotSource = CallbackState.Pending;
+                // Wait for the operation to complete
+                MainLoop.Instance.Wait();
 
-                // Lock the mainloop
-                _mainLoop.Lock();
+            }
 
-                // Call the native get_source_info native function passing in the name and callback
-                pa_operation* op = pa_context.pa_context_get_source_info_by_index(pa_Context, index, &SourceCallback, null);
+            // Dereference the pa_operation
+            pa_operation_unref(op);
 
-                // Wait for the callback to signal
-                while (gotSource == CallbackState.Pending)
-                    _mainLoop.Wait();
+            // Unlock the mainloop
+            MainLoop.Instance.Unlock();
 
-                // If the callback returned data
-                if (gotSource == CallbackState.HasData)
-                {
-                    // Copy the unmanaged source_info structure into a SourceInfo object
-                    info = SourceInfo.Convert(*source_info);
+            // Unlock the context
+            Monitor.Exit(this);
 
-                    // Allow PulseAudio to free the source_info
-                    _mainLoop.Accept();
+            // Return the SourceInfo object
+            return info;
 
-                    // Wait for the operation to complete
-                    _mainLoop.Wait();
-
-                }
-
-                // Dereference the pa_operation
-                pa_operation.pa_operation_unref(op);
-
-                // Unlock the mainloop
-                _mainLoop.Unlock();
-
-                // Unlock the context
-                Monitor.Exit(this);
-
-                // Return the SourceInfo object
-                return info;
-            });
         }
 
         /// <summary>
         /// Gets the list of sources on currently on the server
         /// </summary>        
         /// <returns>List of SourceInfo objects</returns>
-        public Task<IReadOnlyList<SourceInfo?>> GetSourceInfoListAsync()
+        public Task<IReadOnlyList<SourceInfo?>> GetSourceInfoListAsync() => Task.Run(GetSourceInfoList);
+
+        /// <summary>
+        /// Gets the list of sources on currently on the server
+        /// </summary>        
+        /// <returns>List of SourceInfo objects</returns>
+        public IReadOnlyList<SourceInfo?> GetSourceInfoList()
         {
 
-            if (_mainLoop == null)
-                throw new InvalidOperationException("MainLoop is null");
+            List<SourceInfo?> sources = new();
 
-            return Task.Run(() =>
+            // Lock the context so that we can remain thread-safe
+            Monitor.Enter(this);
+
+            // Lock the mainloop
+            MainLoop.Instance.Lock();
+
+            pa_source_info* source_info = null;
+
+            // Call the native get_source_info native function passing in the name and callback
+            pa_operation* op = pa_context.pa_context_get_source_info_list(pa_Context, &SourceCallback, &source_info);
+
+            do
             {
-                List<SourceInfo?> sources = new();
+            
+                // Wait for the callback to signal
+                while (pa_operation_get_state(op) == Enums.OperationState.RUNNING && source_info == null)
+                    MainLoop.Instance.Wait();
 
-                // Lock the context so that we can remain thread-safe
-                Monitor.Enter(this);
 
-                // Lock the mainloop
-                _mainLoop.Lock();
-
-                do
+                // If the callback returned data
+                if (source_info != null)
                 {
-                    // Set flag to 'pending'
-                    gotSource = CallbackState.Pending;
+                    // Copy the unmanaged source_info structure into a SourceInfo object
+                    // and add to the list
+                    sources.Add(SourceInfo.Convert(*source_info));
 
-                    // Call the native get_source_info native function passing in the name and callback
-                    pa_operation* op = pa_context.pa_context_get_source_info_list(pa_Context, &SourceCallback, _mainLoop.ptr);
+                    // Allow PulseAudio to free the source_info
+                    MainLoop.Instance.Accept();
 
-                    // Wait for the callback to signal
-                    while (gotSource == CallbackState.Pending)
-                        _mainLoop.Wait();
+                    source_info = null;
 
-
-                    // If the callback returned data
-                    if (gotSource == CallbackState.HasData)
-                    {
-                        // Copy the unmanaged source_info structure into a SourceInfo object
-                        // and add to the list
-                        sources.Add(SourceInfo.Convert(*source_info));
-
-                        // Allow PulseAudio to free the source_info
-                        _mainLoop.Accept();
-                        _mainLoop.Wait();
-
-                        // Dereference the pa_operation
-                        pa_operation.pa_operation_unref(op);
-
-                        continue;
-                    }
-
-                    // Dereference the pa_operation
-                    pa_operation.pa_operation_unref(op);
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
 
 
-                } while (gotSource != CallbackState.Success);
+            } while (true);
+
+            // Dereference the pa_operation
+            pa_operation_unref(op);
 
 
+            // Unlock the mainloop
+            MainLoop.Instance.Unlock();
 
-                // Unlock the mainloop
-                _mainLoop.Unlock();
 
+            // Unlock the context
+            Monitor.Exit(this);
 
-                // Unlock the context
-                Monitor.Exit(this);
+            // Return the SinkInfo object
+            return sources;
 
-                // Return the SinkInfo object
-                return (IReadOnlyList<SourceInfo?>)sources;
-            });
         }
         #endregion
 
